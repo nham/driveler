f63bdea2188b96eab1d9bfe6a6c12c5c8a3dcef3
nham <hamann.nick@gmail.com>
2013-06-09 15:09:21 +0000

    add logging and command line  options for reporting levels.

--- driveler.py
+++ driveler.py
@@ -1,11 +1,13 @@
-import sys
 import os, shutil, subprocess
+import logging
+import argparse
 
 fname_no_ext = lambda fn: fn[:fn.index('.')]
 
 class Driveler:
     def __init__(self):
-        pass
+        args = self.parse_arguments()
+        logging.basicConfig(level=args.verbosity)
 
     def out_path(self, relpath):
         return self.out_folder + relpath
@@ -26,6 +28,7 @@ class Driveler:
                       '--title-prefix', self.site_title]
 
         p = subprocess.call(pandoc_call)
+        logging.info('generated page {0}'.format(out_file))
         return
 
 
@@ -50,19 +53,43 @@ class Driveler:
 
         for fname in os.listdir(folder):
             if self.is_page(fname) and filter(fname):
-                if not os.path.exists(new_out_folder):
-                    os.makedirs(new_out_folder)
-
+                self.create_folder_if_not_exists(new_out_folder)
                 self.convert_file(folder, fname)
 
+
+    def create_folder_if_not_exists(self, folder):
+        if not os.path.exists(folder):
+            logging.info('folder {0} does not exist, creating'.format(folder))
+            os.makedirs(folder)
+            
 
     def copy_folder(self, folder):
         new_out_folder = self.out_path(folder)
 
-        if not os.path.exists(new_out_folder):
-            os.makedirs(new_out_folder)
-            
+        self.create_folder_if_not_exists(new_out_folder)
+
         for fname in os.listdir(folder):
             site_file = folder + fname
             out_file =  new_out_folder + fname
             shutil.copy(site_file, out_file)
+            logging.info('copied {0} to {1}'.format(site_file, out_file))
+
+
+    def parse_arguments(self):
+        parser = argparse.ArgumentParser(description="""A stupid 
+        static site generator/glorified pandoc wrapper.""",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+        parser.add_argument('-v', '--verbose', action='store_const',
+            const=logging.INFO, dest='verbosity',
+            help='Show all messages.')
+
+        parser.add_argument('-q', '--quiet', action='store_const',
+            const=logging.CRITICAL, dest='verbosity',
+            help='Show only critical errors.')
+
+        parser.add_argument('-D', '--debug', action='store_const',
+            const=logging.DEBUG, dest='verbosity',
+            help='Show all message, including debug messages.')
+
+        return parser.parse_args()
